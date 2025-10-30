a9034b462d02960b8fb307c88ada4e439768b071
mawucano-design <mawucano@gmail.com>
2025-10-30 11:29:51 -0300

    Create gee_utils.py

---
+++ utils/gee_utils.py
@@ -0,0 +1,9 @@
+# Placeholder for Google Earth Engine functions
+def initialize_earth_engine():
+    return False
+
+def get_sentinel2_image(geometry, start_date, end_date, cloud_cover=20):
+    return None, None
+
+def extract_satellite_values(gdf, image_s2):
+    return None
