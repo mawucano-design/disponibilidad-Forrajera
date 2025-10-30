5618e7e5a06156e8d0b3080cdc30a7a6feeae32f
mawucano-design <mawucano@gmail.com>
2025-10-30 11:51:40 -0300

    Delete utils directory

--- utils/gee_utils.py
+++
@@ -1,9 +0,0 @@
-# Placeholder functions
-def initialize_earth_engine():
-    return False
-
-def get_sentinel2_image(geometry, start_date, end_date, cloud_cover=20):
-    return None, None
-
-def extract_satellite_values(gdf, image_s2):
-    return None
